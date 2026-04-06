d0296e8ab4e50c379b32a301f86113f41a556347
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2026-04-06 14:42:42 +0200

    Import FoundationNetworking in test files
    Conditionally import FoundationNetworking in tests to support platforms (e.g. Linux) where networking APIs live outside Foundation. Added `#if canImport(FoundationNetworking) import FoundationNetworking #endif` to APIClientTests.swift, MockURLHandlerStore.swift, and MockURLProtocol.swift.

--- Tests/KiteTests/Mocks/MockURLHandlerStore.swift
+++ Tests/KiteTests/Mocks/MockURLHandlerStore.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 final actor MockURLHandlerStore {
     private typealias Handler = @Sendable (URLRequest) throws -> (Data, URLResponse)
